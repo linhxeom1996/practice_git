c9855cc5e203487fae623f6de8a8793abfad0f09
Nguyễn Đình Linh <linhnguyendeveloper@maplelabs.co>
2022-10-13 00:11:16 +0700

    add title appBar

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
- 
+      title: "My App",
+      home: HomePage(),
+    );
+  }
+}
